0014e1d24c5f556e7a7ecb809e14df49fdc01fdc
CozyHome <gaunletgames@gmail.com>
2021-10-24 23:30:03 +0100

    Lorem ipsum for the page!

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    	<div className = "article-body">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa id neque aliquam vestibulum. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi. Aliquet bibendum enim facilisis gravida neque convallis. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Sed enim ut sem viverra aliquet eget sit amet tellus. Nibh mauris cursus mattis molestie a iaculis at. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Ut ornare lectus sit amet est placerat in egestas. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Cursus metus aliquam eleifend mi in nulla posuere. Id semper risus in hendrerit gravida. Quis risus sed vulputate odio. Viverra orci sagittis eu volutpat odio.
+			</p>
+		</div>
+	</div>
   );
 }
 
