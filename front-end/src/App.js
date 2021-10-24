6c8277c959b7797c49d03a4592154a58b8fd846c
CozyHome <gaunletgames@gmail.com>
2021-10-24 23:37:19 +0100

    is this going to actually commit?

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
     	<div className = "article-body">
 			<p>
-				Testing 123
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa id neque aliquam vestibulum. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi. Aliquet bibendum enim facilisis gravida neque convallis. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Sed enim ut sem viverra aliquet eget sit amet tellus. Nibh mauris cursus mattis molestie a iaculis at. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Ut ornare lectus sit amet est placerat in egestas. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Cursus metus aliquam eleifend mi in nulla posuere. Id semper risus in hendrerit gravida. Quis risus sed vulputate odio. Viverra orci sagittis eu volutpat odio.
 			</p>
 		</div>
