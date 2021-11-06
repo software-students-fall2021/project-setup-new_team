230d23445eb1dfb0e5a747e58ead532aad545e9a
adp527 <adp@DESKTOP-Q7MNEKE.localdomain>
2021-11-06 02:15:48 -0400

    Updating back-end query support

--- back-end/app.js
+++ back-end/app.js
@@ -12,14 +12,25 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public')) // only need this when we add static files: dead code for now
 
 
-app.get('/articles/:name', (req,res,next) => {
-    axios.get(`https://my.api.mockaroo.com/articles.json?key=02fe4bc0&id=${req.params.name}`)
+app.get('/articles_data/:id', (req,res,next) => {
+    axios.get(`https://my.api.mockaroo.com/articles.json?key=02fe4bc0&type=article&id=${req.params.id}`) //random mock data
     .then(response => res.json(response.data))
     .catch(err => next(err))
 })
 
+app.get('/games_data/:id', (req,res,next) => {
+    axios.get(`https://my.api.mockaroo.com/articles.json?key=02fe4bc0&type=game&id=${req.params.id}`) //random mock data
+    .then(response => res.json(response.data))
+    .catch(err => next(err))
+})
+
+app.get('/images/:id', (req,res,next)  => {
+    axios.get(`https://picsum.photos/id/${req.params.id}/info`) //will return JSON for _specific photo_ from picsum
+    .then(response => res.json(response.data))
+    .catch(err => next(err))
+})
 
 module.exports = app
