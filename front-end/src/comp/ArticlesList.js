67f1d956794ef75dc16ac08f8ec4cd6dcd6d5f46
CozyHome <gaunletgames@gmail.com>
2021-11-08 01:37:28 +0000

    integrating the front-end with the back-end to allow for comment posting DC @ 8:37 PM Nov. 7th 2021

--- front-end/src/comp/ArticlesList.js
+++ front-end/src/comp/ArticlesList.js
@@ -6,9 +6,9 @@ const ArticlesList= (props) => {
     return (
         <Stack spacing={2}> 
             {props.a_data.slice(0, 10).map((article, key) => (
-                <ArticleCard key={key} a_title = {article.title} 
-                                       a_tags = {article.username} 
-                                       a_desc = {article.lorem} />
+                <ArticleCard key={key} article_name = {article.article_name} 
+                                       article_poster = {article.article_poster} 
+                                       article_text = {article.article_text} />
             ))}
         </Stack>
     );
--- front-end/src/comp/CommentCard.js
+++ front-end/src/comp/CommentCard.js
@@ -6,17 +6,17 @@ import Rating from '@mui/material/Rating';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-const CommentCard = ({ username, rating, text }) => 
+const CommentCard = ({ username, rating, comment }) => 
 {
     return (
-        <Card sx = {{ my: 3, minHeight: 160, justifySelf: "center", maxWidth: 400 }} className = "card">
+        <Card sx = {{ my: 3, minHeight: 120, justifySelf: "center", maxWidth: 400 }} className = "card">
             <CardContent>
-            <Rating name="read-only" value={rating / 20} readOnly sx ={{ paddingBottom: .5}}/>
+            <Rating name="read-only" value={rating} readOnly sx ={{ paddingBottom: .5}}/>
             <Typography variant="h5" color="text.secondary">
                     { username }
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    { text.substring(0, 150) }
+                    { comment }
                 </Typography>
             </CardContent>
         </Card>
