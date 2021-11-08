456d2a50516abf64e46446bf48020545f66e6242
robertguerrer <58481306+robertguerrer@users.noreply.github.com>
2021-11-07 23:13:38 -0500

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const server = require("./app") // load up the web server
 const port = 3000 // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
