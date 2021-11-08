9a1db9fe5bc31d4afb9c4085aaa68f139089ede1
CozyHome <gaunletgames@gmail.com>
2021-11-08 02:28:08 +0000

    allowing for dynamic back-end refresh every time the articles page is viewed

--- front-end/src/comp/ArticlesList.js
+++ front-end/src/comp/ArticlesList.js
@@ -5,7 +5,7 @@ import ArticleCard from '../comp/ArticleCloud'
 const ArticlesList= (props) => {
     return (
         <Stack spacing={2}> 
-            {props.a_data.slice(0, 10).map((article, key) => (
+            { props.a_data.slice(0, 10).map((article, key) => (
                 <ArticleCard key={key} article_name = {article.article_name} 
                                        article_poster = {article.article_poster} 
                                        article_text = {article.article_text} />
--- front-end/src/page/Article_P.js
+++ front-end/src/page/Article_P.js
@@ -2,13 +2,13 @@ import axios         from 'axios';
 import React, { useState, useEffect } from 'react'
 import { styled }    from '@mui/system';
 import { useParams } from "react-router-dom";
+import Divider       from "@mui/material/Divider"
 import Rating        from '@mui/material/Rating';
 import Button        from '@mui/material/Button';
 import Typography    from '@mui/material/Typography';
 import TextField     from '@mui/material/TextField';
 
 import SendIcon      from '@mui/icons-material/Send';
-// import CommentJSON   from './../data/var-game-comments.json';
 import CommentList   from './../comp/CommentList';
 
 import './Article.css'
@@ -45,8 +45,7 @@ const Article_P = (props) => {
                 setArticleData(response.data.data))
         .catch((err) => 
         {
-            // error! bad bad bad!  
-            console.log(`uh oh! Can't request anymore data man :(`)
+            // article doesn't exist! very bad.
             console.error(err)
         })
     }, [])
@@ -65,7 +64,9 @@ const Article_P = (props) => {
                 <Typography variant="body1" component = "div" className = "header" sx= {{ py: .5, textAlign: "left"}}>
                     By: {article.poster_name}
                 </Typography>
-                <Rating name="read-only" value={article.rating / 20} readOnly sx ={{ paddingBottom: .5}}/>
+                <Rating name="read-only" value={article.rating / 20} readOnly sx ={{ paddingBottom: .5}}/>    
+                <img src= { article.image_url } />
+                <Divider sx ={{ my: 1 }}/>
                 <Typography variant="body2" component = "div" className = "header" sx ={{paddingBottom: 2}}>
                         {article.article_text}
                 </Typography>
